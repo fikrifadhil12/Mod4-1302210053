8c799bccc0114a95939b8cabb3052bfa6cf09e3a
unknown <fikri.fm12@gmail.com>
2023-03-12 20:27:36 +0700

    menambah implementasi dengan table-driven

--- tpmod4-1302210053/tpmod4-1302210053/Program.cs
+++ tpmod4-1302210053/tpmod4-1302210053/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class program
+{
+    enum kelurahan
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+
+    }
+
+    private static int kodepos(kelurahan inputkodepos)
+    {
+        int[] outputkodepos = {40266,40287,40267,40256,40287,40286,40286,40286,40272,40274,40273 };
+        int inputint =(int)inputkodepos;
+
+        return outputkodepos[inputint];
+    }
+
+    public static void Main(string[] args)
+    {
+        kelurahan kelurahan = kelurahan.Maleer;
+        int nokodepos = kodepos(kelurahan);
+        Console.WriteLine(kelurahan + "  " + nokodepos);
+
+    }
+
+}
+
+
+
+
+
+
+
